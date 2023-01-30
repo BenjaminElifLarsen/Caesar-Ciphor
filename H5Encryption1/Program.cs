68b79b855a49a06a763bafc43accd8b4a074f0f3
Benjamin <beela93@hotmail.com>
2023-01-30 11:10:12 +0100

    started work on freq_analysis

--- H5Encryption1/Program.cs
+++ H5Encryption1/Program.cs
@@ -24,8 +24,49 @@ Console.WriteLine(se2);
 //Console.WriteLine(Caesar.Decrypt(se2,15));
 
 
+var brutes = CaesarAnalysis.BruteForce(se1);
+foreach(var t in brutes.Keys)
+{
+    Console.WriteLine($"{t}: " + brutes[t]);
+    Console.WriteLine();
+}
+
+var text = se1;
+var key = CaesarAnalysis.FindPossibleKey(text);
+Console.WriteLine($"{key}: " + Caesar.Decrypt(text, key));
+
 namespace Test
 {
+    public static class CaesarAnalysis
+    {
+        public static Dictionary<byte, string> BruteForce(string encrypted)
+        {
+            var dic = new Dictionary<byte, string>();
+            for(byte i = 25; i > 0; i--)
+            {
+                dic.Add(i, Caesar.Decrypt(encrypted, i));
+            }
+            return dic;
+        }
+
+        public static byte FindPossibleKey(string decrypted)
+        {
+            var freqNorm = new double[]
+            { //letters, A-Z
+                0.64297, 0.11746, 0.21902, 0.33483, 1.00000, 0.17541,
+                0.15864, 0.47977, 0.54842, 0.01205, 0.06078, 0.31688, 0.18942,
+                0.53133, 0.59101, 0.15187, 0.00748, 0.47134, 0.49811, 0.71296,
+                0.21713, 0.07700, 0.18580, 0.01181, 0.15541, 0.00583
+            };
+            var noSpace = decrypted.Where(c => c != ' ');
+            var groupping = noSpace.GroupBy(x => x);
+            var mostCommon = groupping.OrderByDescending(x => x.Count()).First().Key;
+            var difference = (byte)(mostCommon - 'E');
+            return difference;
+        }
+    }
+
+
     public static class Caesar
     {
         public static string FormatMessage(string message)
