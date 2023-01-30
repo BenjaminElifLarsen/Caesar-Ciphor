57dfe9f34000ad162d4569b5a180f98ab5e31665
Benjamin <beela93@hotmail.com>
2023-01-30 09:59:59 +0100

    added some song text files which a part of the task required

--- H5Encryption1/Program.cs
+++ H5Encryption1/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System.Text.RegularExpressions;
 using Test;
 
-var f = Caesar.FormatMessage("ab ^ c | z");
-var e = Caesar.Encrypt(f, 15);
-var d = Caesar.Decrypt(e, 15);
-Console.WriteLine(f);
-Console.WriteLine(e);
-Console.WriteLine(d);
+//var f = Caesar.FormatMessage("ab ^ c | z");
+//var e = Caesar.Encrypt(f, 15);
+//var d = Caesar.Decrypt(e, 15);
+//Console.WriteLine(f);
+//Console.WriteLine(e);
+//Console.WriteLine(d);
+
+
+
+
+var sf1 = Caesar.FormatMessage(File.ReadAllText("song 1.txt"));
+var se1 = Caesar.Encrypt(sf1, 4);
+Console.WriteLine(se1);
+//Console.WriteLine(Caesar.Decrypt(se1, 4));
+
+Console.WriteLine();
+
+var sf2 = Caesar.FormatMessage(File.ReadAllText("song 2.txt"));
+var se2 = Caesar.Encrypt(sf2, 15);
+Console.WriteLine(se2);
+//Console.WriteLine(Caesar.Decrypt(se2,15));
 
 
 namespace Test
